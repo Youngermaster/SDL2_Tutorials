0b30f5d90cb3752717a85a097840c06ff96389e8
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-05-23 20:45:21 -0500

    Path error fixed

--- lazyfoo.net/06_extension_libraries_and_loading_other_image_formats/06_extension_libraries_and_loading_other_image_formats.cpp
+++ lazyfoo.net/06_extension_libraries_and_loading_other_image_formats/06_extension_libraries_and_loading_other_image_formats.cpp
@@ -65,7 +65,7 @@ bool loadMedia() {
     bool success = true;
 
     //Load PNG surface
-    gPNGSurface = loadSurface("06_extension_libraries_and_loading_other_image_formats/loaded.png");
+    gPNGSurface = loadSurface("Logo.png");
     if (gPNGSurface == NULL) {
         printf("Failed to load PNG image!\n");
         success = false;
